498fcf4d9888903182bea546da697efc94635780
Davolkoff <daniil.volkov.02@yandex.ru>
2022-05-21 19:20:10 +0700

    Update Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -11,16 +11,14 @@ contract MyStaking {
     IERC20 rewardsToken; // rewards token
     uint  rewardsFrequency; // how often are rewards generated
 
-    struct Stake {
-        uint amount; // amount of new stake
-        uint timestamp; // when stake was proceeded
-    }
-
     struct User {
         uint lpBalance; // needs to count rewards
+        uint availableBalance; // balance, available to withdraw
+        uint lastStakeTimestamp; // timestamp, when user staked tokens last time
+        uint reservedReward; // reward, creating by staking with old LP balance
+
         uint rewardsBalance; // balance of rewards tokens of user
         uint lastRewardsTime; // needs to recount rewardsBalance if lpBalance changes
-        Stake [] stakes; // needs to know, how many lp tokens user can withdraw at the moment
     }
 
     mapping (address => User) users; // information about users
@@ -55,33 +53,50 @@ contract MyStaking {
         _;
     }
 
-    modifier countRewards {
+    modifier countBalances() {
+        // adding reserved tokens to available balance
+        if (block.timestamp - users[msg.sender].lastStakeTimestamp > freezeTime) {
+            users[msg.sender].availableBalance = users[msg.sender].lpBalance;
+        }
+        // counting rewards tokens
         if (users[msg.sender].lastRewardsTime != 0) {
             uint rewardsPerCycle = users[msg.sender].lpBalance * rewardsPercent / 100;
-            uint numberOfCycles = (block.timestamp - users[msg.sender].lastRewardsTime) / rewardsFrequency; 
-
-            users[msg.sender].rewardsBalance += (rewardsPerCycle * numberOfCycles);
+            uint numberOfCycles = ((block.timestamp - users[msg.sender].lastRewardsTime) / rewardsFrequency);
+            
+            if (users[msg.sender].reservedReward != 0 && numberOfCycles > 0) {
+                numberOfCycles -= 1;
+                users[msg.sender].rewardsBalance += (rewardsPerCycle * numberOfCycles);
+                users[msg.sender].rewardsBalance += users[msg.sender].reservedReward;
+                users[msg.sender].reservedReward = 0;
+            }
+            else {
+                users[msg.sender].rewardsBalance += (rewardsPerCycle * numberOfCycles);
+            }
+            
         }
-
+        
         users[msg.sender].lastRewardsTime = block.timestamp;
         _;
     }
 
     // sends tokens to contract for staking
-    function stake (uint _amount) external countRewards {
+    function stake (uint _amount) external countBalances {
         require(_amount > 0, "You can't send 0 tokens");
-        // adding info about this stake
-        users[msg.sender].stakes.push(Stake(_amount, block.timestamp));
+
+        // counting rewards for old LP balance
+        uint rewardsPerCycle = users[msg.sender].lpBalance * rewardsPercent / 100;
+        users[msg.sender].reservedReward += rewardsPerCycle;
 
         // before it user should approve tokens to contract
         lpToken.transferFrom(msg.sender, address(this), _amount);
         users[msg.sender].lpBalance += _amount;
+        users[msg.sender].lastStakeTimestamp = block.timestamp;
         
         emit StakeEv(msg.sender, _amount);
     }
    
     // sends reward tokens to user
-    function claim () external countRewards {
+    function claim () external countBalances {
         require(users[msg.sender].rewardsBalance > 0, "You haven't got reward tokens");
         rewardsToken.transfer(msg.sender, users[msg.sender].rewardsBalance);
 
@@ -90,44 +105,20 @@ contract MyStaking {
     }
 
     // withdraw lp tokens
-    function unstake () external countRewards {
+    function unstake () external countBalances {
         require(users[msg.sender].lpBalance > 0, "Nothing to unstake");
-        uint availableBalance;
-        
-        // for sorting array
-        uint lastUnusedElement;
-        uint elementsUsed;
+        require(users[msg.sender].availableBalance > 0, "You can't unstake lp tokens right now");
 
-
-        // collects stakes, that has been staked more than freeze time ago and sorts array
-        for (uint i=0; i<users[msg.sender].stakes.length; ++i) {
-            if (block.timestamp - users[msg.sender].stakes[i].timestamp > freezeTime) {
-                availableBalance += users[msg.sender].stakes[i].amount;
-                users[msg.sender].stakes[i].amount = 0;
-                
-                elementsUsed++;
-            }
-            else {
-                users[msg.sender].stakes[lastUnusedElement] = users[msg.sender].stakes[i];
-                lastUnusedElement++;
-            }
-            // now unused elements in the left side of array
-        }
-
-        for (uint i = 0; i<elementsUsed; i++) {
-            users[msg.sender].stakes.pop();
-        }
-
-        require(availableBalance > 0, "You can't unstake lp tokens right now");
-
-        lpToken.transfer(msg.sender, availableBalance);
-        users[msg.sender].lpBalance -= availableBalance;
+        lpToken.transfer(msg.sender, users[msg.sender].availableBalance);
+        users[msg.sender].lpBalance -= users[msg.sender].availableBalance;
 
         emit UnstakeEv(
             msg.sender,
-            availableBalance,
+            users[msg.sender].availableBalance,
             users[msg.sender].lpBalance
         );
+
+        users[msg.sender].availableBalance = 0;
     }
 
     // changes settings only by owner
